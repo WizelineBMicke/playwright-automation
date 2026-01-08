99b16a90c3198473d8396d6c19ddbd06d1fd379d
Miguel Chan <80924143+WizelineBMicke@users.noreply.github.com>
2026-01-08 17:41:16 -0600

    Adding home test, runtime env configs

---
+++ utils/envConfig.ts
@@ -0,0 +1,14 @@
+import * as environments from '../data/environments.json';
+
+// Define the available environment keys
+type envConfig = 'dev' | 'staging' | 'prod';
+
+// Get the environment from the system variable, default to 'staging'
+const currentEnv = (process.env.APP_ENV as envConfig) || 'staging';
+
+// Export the configuration for the current environment
+export const ENV_CONFIG = {
+    BASE_URL: environments[currentEnv].baseURL,
+    API_URL: environments[currentEnv].apiURL,
+    NAME: currentEnv
+};
